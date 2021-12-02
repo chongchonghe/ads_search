35b42bade3d857366bcf1f6796c5966fa028d433
ChongChong He <he.cc.phy@gmail.com>
2021-12-02 15:10:25 -0500

    macOS: 2021-12-02 15:10:25

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 A simple NASA ADS query tool that works.
 
-## Key Features
+<!-- ## Key Features -->
 
-- Copy bibtex into clipboard or open the ads entry in browser. (Tested on macOS)
-- Possibly the easiest workflow possible: copy a MNRAS/ApJ style reference, e.g. `1955, ApJ, 121, 161`, as the arguments of the `ads` command, and the bibtex entry will be copied into your clipboard.
+<!-- - Copy bibtex into clipboard or open the ads entry in browser. (Tested on macOS) -->
+<!-- - Possibly the easiest workflow possible: copy a MNRAS/ApJ style reference, e.g. `1955, ApJ, 121, 161`, as the arguments of the `ads` command, and the bibtex entry will be copied into your clipboard. -->
 
-## How to Use
+## Installation
+
+Clone this repository and install it via `pip`:
 
 To clone and use this application, you'll need [Git](https://git-scm.com) and Python installed on your computer. From your command line:
 
@@ -18,23 +20,50 @@ $ git clone https://github.com/chongchonghe/ads_search.git
 # Go into the repository
 $ cd ads_search
 
-# Install adssearch
-$ make
-
 # OR, alternatively,
 $ pip install -e .
 
-# Run the test
+# Run a test
 $ ads -t
 ```
 
-Example use:
+## Usage
 
 ```bash
->>> ads 1955, ApJ, 121, 161
->>> ads 2002, A\&A, 382, 28
->>> ads 2002, 'A&A' 382, 28
->>> ads -w 1955, ApJ, 121, 161
+>>> ads
+usage: ads [-h] [-b BIBLIOGRAPHY BIBLIOGRAPHY BIBLIOGRAPHY BIBLIOGRAPHY] [-ay AY [AY ...]] [-a ARTICLE_IDENTIFIER] [-nob] [-t]
+
+A simple ads parser that works.
+
+Can do one of the following:
+1. Find the url of a paper on ADS witha given a ApJ/MNRAS style bibliography
+2. Find the url of a paper on ADS witha given the author and year
+3. Add a article to BibDesk given a article identifier
+
+Author: Chong-Chong He (che1234@umd.edu)
+
+Examples:
+>>> ads -ay Salpeter55
+>>> ads -ay Salpeter1955
+>>> ads -ay Salpeter 1955
+>>> ads -b 1955, ApJ, 121, 161
+>>> ads -a 1955ApJ...121..161S
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -b BIBLIOGRAPHY BIBLIOGRAPHY BIBLIOGRAPHY BIBLIOGRAPHY
+                        Open a browser tab and direct to an ADS page of the given article specified by a ApJ/MNRAS style bibliography. The tracing commas will be ignored. Replace '&' with '\&' in the journal name. e.g.
+                        	ads -b 1955, ApJ, 121, 161
+                        	ads -b 2002, A\&A, 382, 28
+  -ay AY [AY ...]       Open a browser tab and direct to an ADS page of the given article specified by first author and year. Two-digit number are idendified as 19xx if bigger than 50 else as 20xx. e.g.
+                        	ads -ay Salpeter 1955
+                        	ads -ay Salpeter1955
+                        	ads -ay Salpeter55.
+  -a ARTICLE_IDENTIFIER
+                        Add article to bibdesk. Accept bibcode/arxiv id/doi as the article_identifier. Equivalent to 'ads2bibdesk ARTICLE_IDENTIFIER'. e.g.
+                        	ads -a 1955ApJ...121..161S
+  -nob                  Toggle off 'open url in borwser'
+  -t                    Run a test: 'ads -b 1955, ApJ, 121, 161'
 ```
 
 ## Credits
@@ -54,4 +83,5 @@ Chong-Chong He ([che1234@umd.edu]())
 
 ## License
 
-MIT
+MITl
+
--- adssearch.py
+++ adssearch.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
-"""
-Copy bibtex to clipboard
-"""
 
 import sys, os
-import subprocess
-import ads
-import warnings
 from argparse import ArgumentParser, RawTextHelpFormatter
+
+def get_ads(year, jour, vol, page):
+
+    if not year.isdigit():
+        print("Invalid YEAR in the input.")
+        print("Usage: {} year journal_name volume Page/ID".format(sys.argv[0]))
+        return
+    if jour=='ApJ' and page[0]=='L':
+        jour = 'ApJL'
+    if jour=='Science':
+        jour = 'Sci'
+    papers = ads.SearchQuery(bibstem=jour, year=year, volume=vol, page=page)
+    return papers
+
+def parse_key_words(args):
+    return [arg.replace(',', '').replace(' ', '') for arg in args]
 
 def parse_args():
 
@@ -27,77 +37,136 @@ Example:
 >>> {0} -w 1955, ApJ, 121, 161""".format(os.path.basename(sys.argv[0])),
         formatter_class=RawTextHelpFormatter)
     # parser.add_argument('-b', type=str, nargs=1, help="bibcode")
-    parser.add_argument('year', nargs='?', help="year")
-    parser.add_argument('jour', nargs='?', help="journal")
-    parser.add_argument('vol', nargs='?', help="volume")
-    parser.add_argument('page', nargs='?', help="page")
+    parser.add_argument('year', help="year")
+    parser.add_argument('jour', help="journal")
+    parser.add_argument('vol', help="volume")
+    parser.add_argument('page', help="page")
     parser.add_argument('-w', action='store_true', help="open url in borwser")
     parser.add_argument('-t', action='store_true',
                         help=("run a test, which is equivalent to running "
                               "'ads 1955, ApJ, 121, 161'"))
     return parser.parse_args()
 
-def openads(bibcode):
-    
-    url = "https://ui.adsabs.harvard.edu/abs/{}".format(bibcode)
-    url = url.replace('&', '\&')
-    os.system('open ' + url)
+def parse_args_v2():
 
-def get_ads(year, jour, vol, page):
+    parser = ArgumentParser(
+        description="""
+A simple ads parser that works.
 
+Can do one of the following:
+1. Find the url of a paper on ADS witha given a ApJ/MNRAS style bibliography
+2. Find the url of a paper on ADS witha given the author and year
+3. Add a article to BibDesk given a article identifier
+
+Author: Chong-Chong He (che1234@umd.edu)
+
+Examples:
+>>> {0} -ay Salpeter55
+>>> {0} -ay Salpeter1955
+>>> {0} -ay Salpeter 1955
+>>> {0} -b 1955, ApJ, 121, 161
+>>> {0} -a 1955ApJ...121..161S""".format(os.path.basename(sys.argv[0])),
+        formatter_class=RawTextHelpFormatter)
+    # ["bibliography", 'year', 'vol', 'page']
+    parser.add_argument('-b', dest="bibliography", nargs=4,
+                        help="Open a browser tab and direct to an ADS page "
+                        "of the given article specified by a ApJ/MNRAS style"
+                        " bibliography. "
+                        "The tracing commas will be ignored. Replace '&' "
+                        "with '\&' in the journal name. e.g.\n"
+                        "\tads -b 1955, ApJ, 121, 161\n"
+                        "\tads -b 2002, A\&A, 382, 28"
+                        )
+    parser.add_argument('-ay', nargs='+',
+                        help="Open a browser tab and direct to an ADS page "
+                        "of the given article specified by first author and "
+                        "year. Two-digit number are idendified as 19xx if bigger"
+                        " than 50 else as 20xx. "
+                        "e.g.\n"
+                        "\tads -ay Salpeter 1955\n"
+                        "\tads -ay Salpeter1955\n"
+                        "\tads -ay Salpeter55.\n")
+    parser.add_argument('-a', dest="article_identifier", nargs=1, type=str,
+                        help="Add article to bibdesk. Accept bibcode/arxiv "
+                        "id/doi as the article_identifier. Equivalent to "
+                        "'ads2bibdesk ARTICLE_IDENTIFIER'. e.g.\n"
+                        "\tads -a 1955ApJ...121..161S")
+    parser.add_argument('-nob', action='store_true',
+                        help="Toggle off 'open url in borwser'")
+    parser.add_argument('-t', action='store_true',
+                        help=("Run a test: 'ads -b 1955, ApJ, 121, 161'"))
+    return parser, parser.parse_args()
+
+def validate_ads(year, jour, vol, page):
     if not year.isdigit():
         print("Invalid YEAR in the input.")
         print("Usage: {} year journal_name volume Page/ID".format(sys.argv[0]))
         return
     if jour=='ApJ' and page[0]=='L':
         jour = 'ApJL'
+        page = page[1:]
     if jour=='Science':
         jour = 'Sci'
-    papers = ads.SearchQuery(bibstem=jour, year=year, volume=vol, page=page)
-    return papers
+    return {"year": year, "jour": jour, "vol": vol, "page": page}
 
-def adsurl(year, jour, vol, page, is_bibcode=False, openurl=False):
+def clean_args(arg):
+    for it in arg.keys():
+        arg[it] = arg[it].replace(',', '').replace(' ', '')
+        pass
+    return arg
 
-    papers = get_ads(year, jour, vol, page)
-    for p in papers:
-        warnings.filterwarnings("ignore")
-        if openurl:
-            openads(p.bibcode)
-            return
-        bib = p.bibtex if not is_bibcode else p.bibcode
-        subprocess.run("pbcopy", universal_newlines=True, input=bib, )
-        print('{} copied to clipboard:'.format(
-            'bibcode' if is_bibcode else 'bibtex'))
-        print()
-        print(bib, end='')
-        return
-    print('Failed to find the bibtex. The following search failed:')
-    print(f"ads.SearchQuery(bibstem={jour}, year={year}, "
-          "volume={vol}, page={page})")
-    print(f"Copy the following texts to ads search bar and have a try:")
-    print()
-    print(f'bibstem:"{jour}" year:"{year}" volume:"{vol}" page:"{page}"')
-
-def parse_key_words(args):
-    return [arg.replace(',', '').replace(' ', '') for arg in args]
+def geturl(arg):
+    return "https://ui.adsabs.harvard.edu/search/q=bibstem%3A{jour}%20year%3A{year}%20volume%3A{vol}%20page%3A{page}&sort=date%20desc%2C%20bibcode%20desc&p_=0".format(**arg)
 
 def main():
-    args = parse_args()
-    if args.t:
-        args.year = '1955'
-        args.jour='ApJ'
-        args.vol='121'
-        args.page='161'
-        print("Doing the test: attempting to search for the following article:")
-        print(f"{args.year}, {args.jour}, {args.vol}, {args.page}")
-        print("If you see 'bibtex copied to clipboard:' followed by a bibtex "
-            "entry of the article, 'The Luminosity Function and Stellar Evolution',"
-            " it means the test is successful\n")
-    this_args = parse_key_words([args.year, args.jour, args.vol, args.page])
-    adsurl(*this_args, openurl=args.w)
+    # if len(sys.argv) == 2:
+    #     if sys.argv[1][0] != '-':
+    #         sys.exit(os.system("ads2bibdesk " + sys.argv[1]))
+    # # arg = clean_args(parse_args())
+    parser, parse = parse_args_v2()
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        sys.exit(2)
+    if parse.t:
+        args = {"year": "1955", "jour": "ApJ", "vol": "121", "page": "161"}
+        print("Parsed input:", args)
+        print("opening " + geturl(args))
+        if not parse.nob:
+            os.system("open " + geturl(args))
+            print("This should open a brower tab and direct to Salpeter's "
+                  "1955 paper on ADS")
+    if parse.a is not None:
+        sys.exit(os.system("echo ads2bibdesk " + parse.a))
+        return
+    if parse.b is not None:
+        args = {"year": parse.b[0], "jour": parse.b[1], "vol": parse.b[2], "page": parse.b[3]}
+        args = clean_args(args)         # dict
+        args = validate_ads(**args)     # dict
+        print("Parsed input:", args)
+        print("opening " + geturl(args))
+        if not parse.nob:
+            os.system("open " + geturl(args))
+        # print(get_ads(**arg))
+    if parse.ay is not None:
+        theauthor = parse.ay[0]
+        if theauthor[-2:].isdigit():
+            if theauthor[-4:].isdigit():
+                author = theauthor[:-4]
+                year = theauthor[-4:]
+            else:
+                author = theauthor[:-2]
+                year = int(theauthor[-2:])
+                year = 1900 + year if year >= 50 else 2000 + year
+        else:
+            year = parse.ay[1]
+        print("Parsed input: author = {}, year = {}".format(author, year))
+        # url = "https://ui.adsabs.harvard.edu/search/q=year%3A{year}%20author%3A%22%5E{author}%22&sort=date%20desc%2C%20bibcode%20desc&p_=0".format(year=year, author=author)
+        # The following url contains a filter: Collection:+astronomy
+        url = "'https://ui.adsabs.harvard.edu/search/filter_database_fq_database=AND&filter_database_fq_database=database%3A%22astronomy%22&fq=%7B!type%3Daqp%20v%3D%24fq_database%7D&fq_database=(database%3A%22astronomy%22)&p_=0&q=year%3A{year}%20author%3A%22%5E{author}%22&sort=date%20desc%2C%20bibcode%20desc'".format(year=year, author=author)
+        print("opening " + url)
+        if not parse.nob:
+            os.system("open " + url)
 
 
-if __name__ == "__main__":
-
-    # main(sys.argv)
-    exit(main())
+if __name__ == '__main__':
+    main()
